44645e8f241974b76f62c11d5549e398e86c1bbe
Dinesh "MrInvicto" Verma <itsmrinvicto@gmail.com>
2015-11-11 23:03:54 +0530

    Solution created for Web Typewriter

---
+++ WebTypewriter/WebTypewriter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTypewriter.Startup))]
+namespace WebTypewriter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
